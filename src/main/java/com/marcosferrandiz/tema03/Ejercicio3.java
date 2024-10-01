2f81effd2037b2fe90e39b44f37efd3b9378958e
MarcosFerrandiz <marcos149ft@gmail.com>
2024-10-01 16:36:00 +0200

    Ejericio 3 mayor o menor de edad

---
+++ src/main/java/com/marcosferrandiz/tema03/Ejercicio3.java
@@ -0,0 +1,18 @@
+package com.marcosferrandiz.tema03;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        int edad;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dime tu edad");
+        edad = input.nextInt();
+        if (edad < 18){
+            System.out.println("Eres menor de edad");
+        }
+        else {
+            System.out.println("Eres mayor de edad");
+        }
+    }
+}
