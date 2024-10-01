145cc17b738cec4c6b0053ce70dce97aa944343d
MarcosFerrandiz <marcos149ft@gmail.com>
2024-10-01 17:36:57 +0200

    Ejericio 4 ordenar numeros

---
+++ src/main/java/com/marcosferrandiz/tema03/Ejercicio4.java
@@ -0,0 +1,23 @@
+package com.marcosferrandiz.tema03;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        int numero1;
+        int numero2;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Te voy a preguntar que me digas dos numeros para que los ordene");
+        System.out.println("Dime el primer número");
+        numero1 = input.nextInt();
+        System.out.println("Dime el segundo número");
+        numero2 = input.nextInt();
+
+        if (numero1 > numero2){
+            System.out.println(numero1 + " " + numero2);
+        }
+        else {
+            System.out.println(numero2 + " " + numero1);
+        }
+    }
+}
---
+++ src/main/java/com/marcosferrandiz/tema03/Ejercicio6.java
@@ -0,0 +1,4 @@
+package com.marcosferrandiz.tema03;
+
+public class Ejercicio6 {
+}
