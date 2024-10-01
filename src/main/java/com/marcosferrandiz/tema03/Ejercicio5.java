c8f4037ccd82d72a3c22c34997aa175d1d6ba69a
MarcosFerrandiz <marcos149ft@gmail.com>
2024-10-01 17:38:54 +0200

    Ejericio 5 notas

--- src/main/java/com/marcosferrandiz/tema03/Ejercicio5.java
+++ src/main/java/com/marcosferrandiz/tema03/Ejercicio5.java
@@ -1,4 +1,23 @@
 package com.marcosferrandiz.tema03;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
+    public static void main(String[] args) {
+        int nota;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dime un número del 1 al 10");
+        nota = input.nextInt();
+
+        switch (nota){
+            case 1,2,3,4 -> System.out.println("INSUFICIENTE");
+            case 5 -> System.out.println("SUFICIENTE");
+            case 6 -> System.out.println("BIEN");
+            case 7,8 -> System.out.println("NOTABLE");
+            case 9,10 -> System.out.println("SOBRESALIENTE");
+            default -> System.out.println("ERROR");
+        }
+
+    }
 }
