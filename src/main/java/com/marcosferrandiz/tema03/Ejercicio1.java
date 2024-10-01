76eec65b6a1d923835d9f4e227f49f525cd26712
MarcosFerrandiz <marcos149ft@gmail.com>
2024-10-01 16:19:30 +0200

    Ejericio 1 de par o impar

--- src/main/java/com/marcosferrandiz/tema03/Ejercicio1.java
+++ src/main/java/com/marcosferrandiz/tema03/Ejercicio1.java
@@ -1,9 +1,19 @@
 package com.marcosferrandiz.tema03;
 
+
+import java.util.Scanner;
+
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("Ejercicio 1");
-        System.out.println("Hola buenas tardes");
-
+        float numero;
+        System.out.println("Indíque un número y te diré si es par o impar");
+        Scanner input = new Scanner(System.in);
+        numero = input.nextFloat();
+        if (numero % 2  == 0){
+            System.out.println(numero + " es un numero par");
+        }
+        else{
+            System.out.println(numero + " es un numero impar");
+        }
     }
 }
