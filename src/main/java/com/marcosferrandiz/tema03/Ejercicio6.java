f3c9c692e5df76e40733b1aeb4bf3edc094c56f7
MarcosFerrandiz <marcos149ft@gmail.com>
2024-10-01 17:39:31 +0200

    Ejericio 6 que caos esto de las monedas

--- src/main/java/com/marcosferrandiz/tema03/Ejercicio6.java
+++ src/main/java/com/marcosferrandiz/tema03/Ejercicio6.java
@@ -1,4 +1,39 @@
 package com.marcosferrandiz.tema03;
 
+import java.util.Scanner;
+
 public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float numero;
+        int numeroCts;
+        System.out.println("Inique la cantidad");
+        numero = input.nextFloat();
+        numeroCts = (int)(numero * 100);
+
+        int moneda200 = numeroCts / 200;
+        int resto = numeroCts % 200;
+
+        int moneda100 = resto / 100;
+        resto = resto % 100;
+
+        int moneda50 = resto / 50;
+        resto = resto % 50;
+
+        int moneda20 = resto / 20;
+        resto = resto % 20;
+
+        int moneda10 = resto / 10;
+        resto = resto % 10;
+
+        int moneda5 = resto / 5;
+        resto = resto % 5;
+
+        int moneda2 = resto / 2;
+        resto = resto % 2;
+
+        int moneda1 = resto;
+
+        System.out.println("La cantidad mínima de monedas de " + numero + " es de " + moneda200 + " monedas de 2 euro, " + moneda100 + " monedas de 1 euro, " + moneda50 + " monedas de 50 cts, " + moneda20 + " monedas de 20 cts, " + moneda10 + " monedas de 10" + moneda5 + " monedas de 5cts, " + moneda2 + " monedas de 2cts," + moneda1 + " monedas de 1cts");
+    }
 }
