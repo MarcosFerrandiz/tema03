e5d07515f9c805cf32eaa16bce892ddf19c1982c
MarcosFerrandiz <marcos149ft@gmail.com>
2024-10-02 15:46:44 +0200

    Update Ejercicio2.java

--- src/main/java/com/marcosferrandiz/tema03/Ejercicio2.java
+++ src/main/java/com/marcosferrandiz/tema03/Ejercicio2.java
@@ -6,7 +6,7 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         float primero;
         float segundo;
-
+        
         Scanner input = new Scanner(System.in);
         System.out.println("Te voy a preguntar que me dígas dos numeros para decirte si el primero es mayor o menor al segundo");
         System.out.println("Dime el primer número");
@@ -19,7 +19,12 @@ public class Ejercicio2 {
             System.out.println("El primér número es mayor al segundo");
         }
         else {
-            System.out.println("El priér número es menor al segundo");
+            if (primero<segundo) {
+                System.out.println("El priér número es menor al segundo");
+            }
+            else {
+                System.out.println("Los numeros són iguales");
+            }
         }
     }
 }
