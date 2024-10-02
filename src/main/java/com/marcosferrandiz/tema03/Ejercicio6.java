acb7a7e9d98164c885cc0986ea6466a1a8037fb6
MarcosFerrandiz <marcos149ft@gmail.com>
2024-10-02 15:26:00 +0200

    Ejericio 6 que caos esto de las monedas

--- src/main/java/com/marcosferrandiz/tema03/Ejercicio6.java
+++ src/main/java/com/marcosferrandiz/tema03/Ejercicio6.java
@@ -13,7 +13,6 @@ public class Ejercicio6 {
 
         int moneda200 = numeroCts / 200;
         int resto = numeroCts % 200;
-
         int moneda100 = resto / 100;
         resto = resto % 100;
 
@@ -25,7 +24,6 @@ public class Ejercicio6 {
 
         int moneda10 = resto / 10;
         resto = resto % 10;
-
         int moneda5 = resto / 5;
         resto = resto % 5;
 
@@ -34,6 +32,6 @@ public class Ejercicio6 {
 
         int moneda1 = resto;
 
-        System.out.println("La cantidad mínima de monedas de " + numero + " es de " + moneda200 + " monedas de 2 euro, " + moneda100 + " monedas de 1 euro, " + moneda50 + " monedas de 50 cts, " + moneda20 + " monedas de 20 cts, " + moneda10 + " monedas de 10" + moneda5 + " monedas de 5cts, " + moneda2 + " monedas de 2cts," + moneda1 + " monedas de 1cts");
+        System.out.println("La cantidad mínima de monedas de " + numero + " es de " + moneda200 + " monedas de 2 euro, " + moneda100 + " monedas de 1 euro, " + moneda50 + " monedas de 50 cts, " + moneda20 + " monedas de 20 cts, " + moneda10 + " monedas de 10 " + moneda5 + " monedas de 5cts, " + moneda2 + " monedas de 2cts," + moneda1 + " monedas de 1cts");
     }
 }
