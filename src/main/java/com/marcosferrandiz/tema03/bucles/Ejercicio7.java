6e12b5374217d3fdb5b5bc358f689f63866c2e94
MarcosFerrandiz <marcos149ft@gmail.com>
2024-10-07 22:51:49 +0200

    Create Ejercicio7.java

---
+++ src/main/java/com/marcosferrandiz/tema03/bucles/Ejercicio7.java
@@ -0,0 +1,35 @@
+package com.marcosferrandiz.tema03.bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float num1;
+        int num2;
+        float resultado = 1;
+
+        System.out.println("Indique un número");
+        num1 = Float.parseFloat(input.nextLine());
+        System.out.println("Indique el número elevado");
+        num2 = Integer.parseInt(input.nextLine());
+
+        input.close();
+
+        if (num2 == 0) {
+            resultado = 1;
+        }
+        if (num1 == 0 && num2 > 0) {
+            resultado = 0;
+        }
+        if (num2 < 0) {
+            num1 = 1 / num1;
+            num2 = -num2;
+        }
+        for (int cuenta = 1; cuenta <= num2; cuenta++) {
+            resultado *= num1;
+
+        }
+        System.out.println(resultado);
+    }
+}
