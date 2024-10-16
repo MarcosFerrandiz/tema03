bd408340186c908cdd3c01160abb8ca186fb7649
MarcosFerrandiz <marcos149ft@gmail.com>
2024-10-16 16:22:25 +0200

    Update Ejercicio7.java

--- src/main/java/com/marcosferrandiz/tema03/ansi/Ejercicio7.java
+++ src/main/java/com/marcosferrandiz/tema03/ansi/Ejercicio7.java
@@ -2,6 +2,7 @@ package com.marcosferrandiz.tema03.ansi;
 
 public class Ejercicio7 {
     public static void main(String[] args) {
+/*
         String cadena = "";
         String cadena2 = "";
 
@@ -13,8 +14,17 @@ public class Ejercicio7 {
                 System.out.printf("%9s%-9s\n", cadena, cadena2);
             }
         }
+*/
+        String cadena = "";
 
 
-
+        for (int i = 1; i <= 9; i++) {
+            cadena += i;
+            System.out.printf("%9s", cadena);
+            for (int a = (cadena.length() - 1); a >= 0; a--) {
+                System.out.print(cadena.charAt(a));
+            }
+            System.out.println();
+        }
     }
 }
