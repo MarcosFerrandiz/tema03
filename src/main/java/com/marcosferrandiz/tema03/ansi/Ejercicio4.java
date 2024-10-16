aca364bc580988c904919ec289797ec6f2ec8bc5
MarcosFerrandiz <marcos149ft@gmail.com>
2024-10-16 15:22:38 +0200

    Create Ejercicio4.java

---
+++ src/main/java/com/marcosferrandiz/tema03/ansi/Ejercicio4.java
@@ -0,0 +1,12 @@
+package com.marcosferrandiz.tema03.ansi;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        String cadena = "";
+
+        for (int i = 1; i <= 9; i++){
+            cadena += i;
+            System.out.println(cadena);
+        }
+    }
+}
